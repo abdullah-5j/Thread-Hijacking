8bea4985147ddca0e69d9bb6c83abadd6ea96cd7
Muhammad Abdullah <2500694@students.au.edu.pk>
2026-07-15 22:55:38 +0500

    Add thread hijack header with function declarations

--- src/Attacker/thread_hijack.h
+++ src/Attacker/thread_hijack.h
@@ -1,1 +1,20 @@
+#pragma once
 
+#ifndef THREAD_HIJACK_H
+#define THREAD_HIJACK_H
+
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <processthreadsapi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <string.h>
+
+extern unsigned char shellcode[];
+extern SIZE_T shellcodeSize;
+
+BOOL GetLocalThreadHandle(DWORD MainThreadID, DWORD* TargetThreadID, HANDLE* HThread);
+BOOL HijackThreadWithStack(HANDLE HThread, PVOID PayloadAddress);
+
+#endif
